943379f33f9c90c38023440471ebc304fbb8ce50
Andrey Soares <76456810+soarexs@users.noreply.github.com>
2023-01-03 16:56:12 -0300

    perf: changing way to sort

--- listafilmes.js
+++ listafilmes.js
@@ -1,17 +1,7 @@
 const moviesHours = [1.04, 1.25, 1.75, 1.9, 2.5]
 
 const findMinDaysToWatch = (moviesToWatch, hoursPerDay) => {
-  const listMovies = moviesToWatch
-
-  for(let i = 0; i < listMovies.length; i++) {
-    for(let j = i + 1; j < listMovies.length; j++) {
-      if(listMovies[i] < listMovies[j]) {
-        const item = listMovies[j]
-        listMovies[j] = listMovies[i]
-        listMovies[i] = item
-      }
-    }
-  }
+  const listMovies = moviesToWatch.sort((a, b) => (b - a))
 
   for(let i = 0; i < listMovies.length; i++) {
     for(let j = i + 1; j < listMovies.length; j++) {
